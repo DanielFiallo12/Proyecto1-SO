065a4a09a2544a5a41b5e9226afe22397a5aa65e
Guillermo Pinzon <guillermopinzon1818@gmail.com>
2024-10-10 18:30:33 -0400

    Arreglar estas clases

--- src/classes/ProductorTarjetas.java
+++ src/classes/ProductorTarjetas.java
@@ -4,6 +4,9 @@
  */
 package classes;
 
+import interfaces.HP;
+import interfaces.Dashboard;
+import interfaces.MSI;
 import java.util.concurrent.Semaphore;
 
 /**
@@ -40,4 +43,71 @@ public class ProductorTarjetas extends Thread {
         this.activo = activo;
     }
     
+    public static int gettarjetasListasH() {
+        return tarjetasListasH;
+    }
+
+    public static void settarjetasListasH(int tarjetasListasH) {
+        ProductorTarjetas.tarjetasListasH -= tarjetasListasH;
+    }
+
+    public static int gettarjetasListasM() {
+        return tarjetasListasM;
+    }
+    public static void settarjetasListasM(int tarjetasListasM) {
+        ProductorTarjetas.tarjetasListasM -= tarjetasListasM;
+    }
+    
+    public void payDayDesarrolladorSprites() {
+        int horasTrabajadas = 24;
+        int salario = sueldoPorHora * horasTrabajadas;
+        if (company == "H") {
+            HPCompany.totalPayH += salario;
+        } else {
+            // Pago de nintendo
+            MSICompany.totalPayM += salario;
+        }
+    }
+
+    public void generarSprite() throws InterruptedException {
+
+        if (company == "H") {
+            //HP
+            if (almacenTarjeta.availablePermits() > 1) {
+                almacenTarjeta.acquire(2);
+                tarjetasListasH += 2;
+                HP.actualizarTarjetasAlmacen(tarjetasListasH);
+            } else if (almacenTarjeta.availablePermits() == 1) {
+                almacenTarjeta.acquire(1);
+                tarjetasListasH += 1;
+                HP.actualizarTarjetasAlmacen(tarjetasListasH);
+            } else {
+                System.out.println("Drive lleno");
+            }
+        } else {
+            //MSI
+            if (almacenTarjeta.availablePermits() > 1) {
+                almacenTarjeta.acquire(1);
+                tarjetasListasM += 1;
+                MSI.actualizarTarjetasAlmacen(tarjetasListasM);
+            } else {
+                System.out.println("Drive lleno");
+            }
+        }
+    }
+
+    @Override
+    public void run() {
+        while (activo) {
+            try {
+
+                Thread.sleep(1000*Dashboard.duracionDias);
+                payDayDesarrolladorSprites();
+                generarSprite();
+
+            } catch (InterruptedException ex) {
+                System.out.println("Metodo Run Desarrollador Sprites Catch");
+            }
+        }
+    }
 }
