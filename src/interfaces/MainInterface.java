9ead1355e9bb1f814f358162fa202be3a77cdd51
Guillermo Pinzon <guillermopinzon1818@gmail.com>
2024-10-10 23:54:22 -0400

    arrreglar interfaces

--- src/interfaces/MainInterface.java
+++ src/interfaces/MainInterface.java
@@ -1,30 +1,49 @@
 
 package interfaces;
 
+import java.awt.Dimension;
+import javax.swing.JPanel;
+
 public class MainInterface extends javax.swing.JFrame {
 
     
      public MainInterface() {
         inittComponents();
+        
+        setLocationRelativeTo(null);
     }
 
          private void inittComponents() {
+             
+           tabbedPane = new javax.swing.JTabbedPane();
 
-        tabbedPane = new javax.swing.JTabbedPane();
+        // Panel para Dashboard con tamaño personalizado
+        JPanel dashboardPanel = new JPanel();
+        dashboardPanel.setPreferredSize(new Dimension(700, 800)); // Tamaño para dashboard
         dashboard1 = new interfaces.Dashboard();
+        dashboardPanel.add(dashboard1);
+
+        // Panel para HP con tamaño personalizado
+        JPanel hpPanel = new JPanel();
+        hpPanel.setPreferredSize(new Dimension(1000, 1000)); // Tamaño para HP
         hp1 = new interfaces.HP();
+        hpPanel.add(hp1);
+
+        // Panel para MSI con tamaño personalizado
+        JPanel msiPanel = new JPanel();
+        msiPanel.setPreferredSize(new Dimension(1000, 1000)); // Tamaño para MSI
         msi2 = new interfaces.MSI();
+        msiPanel.add(msi2);
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+        getContentPane().setLayout(new java.awt.BorderLayout());
 
-        tabbedPane.setName("tabbedPane"); // NOI18N
-        tabbedPane.addTab("Dasboard Principal", dashboard1);
-        tabbedPane.addTab("HP", hp1);
-        tabbedPane.addTab("MSI", msi2);
+        // Añadir las pestañas con los paneles personalizados
+        tabbedPane.addTab("Dasboard Principal", dashboardPanel);
+        tabbedPane.addTab("HP", hpPanel);
+        tabbedPane.addTab("MSI", msiPanel);
 
-getContentPane().setLayout(new java.awt.BorderLayout());
-getContentPane().add(tabbedPane, java.awt.BorderLayout.CENTER);
+        getContentPane().add(tabbedPane, java.awt.BorderLayout.CENTER);
         pack();
     }// </editor-fold>                        
      
